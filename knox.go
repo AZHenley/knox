492b8f5f19a6409810d3d0bd3067a27c7c49ff52
Austin Henley <azh321@gmail.com>
2019-01-18 13:08:37 -0500

    Setup for using lexer and parser.

--- knox.go
+++ knox.go
@@ -1,10 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"knox/lexer"
+	"knox/parser"
+)
 
 func main() {
-	l := New("1 + 1")
+	code, err := ioutil.ReadFile("examples/basic.knox")
+	if err != nil {
+		panic(err)
+	}
+	l := lexer.New(string(code))
+	p := parser.New(l)
+	p.Program()
+	if len(p.Errors()) != 0 {
+		printParserErrors(p.Errors())
+	}
+}
 
-	fmt.Println("HI")
-
+func printParserErrors(errors []string) {
+	for _, msg := range errors {
+		fmt.Println("\t" + msg + "\n")
+	}
 }
