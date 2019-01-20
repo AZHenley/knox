09254aa04cde35a1fed5fa4a87858a4c378069e3
Austin Henley <azh321@gmail.com>
2019-01-20 13:24:38 -0500

    Setup for basic AST.

--- ast/ast.go
+++ ast/ast.go
@@ -2,13 +2,32 @@ package ast
 
 import "knox/token"
 
-// ASTNodeType is a string.
-type ASTNodeType string
+// NodeType is a string.
+type NodeType string
 
-// ASTNode is for the AST.
-type ASTNode struct {
-	nodeType   ASTNodeType
-	children   []ASTNode
+// Node is for the AST.
+type Node struct {
+	nodeType   NodeType
+	children   []Node
 	tokenStart token.Token
-	// symbols symtable.SymTable // Only blocks get a symbol table.
+	//symbols symtable.SymTable // Only blocks get a symbol table.
 }
+
+// Predefined
+const (
+	EXPRESSION     = "EXPRESSION"     // One child. Tree of binary ops, unary ops, and primaries.
+	BINARYOP       = "BINARYOP"       // Two children.
+	UNARYOP        = "UNARYOP"        // One child.
+	VARDECL        = "VARDECL"        // 2-3 children. Third is optional expression for assignment.
+	VARASSIGN      = "VARASSIGN"      // Two children. Name and expression.
+	FUNCDECL       = "FUNCDECL"       // Three children. Name, list of param pairs, list of return pairs
+	IFSTATEMENT    = "IFSTATEMENT"    // Three children. Condition, if block, else block (chain elif/else).
+	FORSTATEMENT   = "FORSTATEMENT"   // Four children. Init, condition, afterthought, block.
+	WHILESTATEMENT = "WHILESTATEMENT" // Two children. Condition and block.
+	JUMPSTATEMENT  = "JUMPSTATEMENT"  // Leaf.
+	VARREF         = "VARREF"         // Leaf.
+	FUNCCALL       = "FUNCCALL"       // Leaf.
+	INT            = "INT"            // Leaf.
+	FLOAT          = "FLOAT"          // Leaf.
+	STRING         = "STRING"         // Leaf.
+)
