af956c90d16bd364df0ed30af61d40e2c7a39960
Austin Henley <azh321@gmail.com>
2019-01-20 14:13:22 -0500

    Return AST.

--- knox.go
+++ knox.go
@@ -1,17 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"knox/lexer"
 	"knox/parser"
 )
 
 func main() {
-	code, err := ioutil.ReadFile("examples/basic.knox")
+	code, err := ioutil.ReadFile("examples/simple.knox")
 	if err != nil {
 		panic(err)
 	}
 	l := lexer.New(string(code))
 	p := parser.New(l)
-	p.Program()
+	ast := p.Program()
+
+	fmt.Println(ast.Children)
 }
