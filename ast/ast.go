1a7416b3fbd8511271d2cf30b7cc78b736e56a17
Austin Henley <azh321@gmail.com>
2019-01-20 13:38:36 -0500

    Fixed problems.

--- ast/ast.go
+++ ast/ast.go
@@ -13,20 +13,23 @@ type Node struct {
 	//symbols symtable.SymTable // Only blocks get a symbol table.
 }
 
-// Predefined
+// Predefined AST node types.
 const (
+	PROGRAM        = "PROGRAM"        // Variable children. One for each funcdecl.
+	BLOCK          = "BLOCK"          // Variable children. One for each statement.
 	EXPRESSION     = "EXPRESSION"     // One child. Tree of binary ops, unary ops, and primaries.
 	BINARYOP       = "BINARYOP"       // Two children.
 	UNARYOP        = "UNARYOP"        // One child.
-	VARDECL        = "VARDECL"        // 2-3 children. Third is optional expression for assignment.
+	VARDECL        = "VARDECL"        // Two or three children. Third is optional expression for assignment.
 	VARASSIGN      = "VARASSIGN"      // Two children. Name and expression.
-	FUNCDECL       = "FUNCDECL"       // Three children. Name, list of param pairs, list of return pairs
+	FUNCDECL       = "FUNCDECL"       // Four children. Name, paramlist for params, paramlist for return, block.
+	PARAMLIST      = "PARAMLIST"      // Variable children. Pairs of name and type.
 	IFSTATEMENT    = "IFSTATEMENT"    // Three children. Condition, if block, else block (chain elif/else).
 	FORSTATEMENT   = "FORSTATEMENT"   // Four children. Init, condition, afterthought, block.
 	WHILESTATEMENT = "WHILESTATEMENT" // Two children. Condition and block.
 	JUMPSTATEMENT  = "JUMPSTATEMENT"  // Leaf.
-	VARREF         = "VARREF"         // Leaf.
-	FUNCCALL       = "FUNCCALL"       // Leaf.
+	VARREF         = "VARREF"         // Variable children. Variable name and list of expressions for array indices.
+	FUNCCALL       = "FUNCCALL"       // Variable children. One expression for each parameter.
 	INT            = "INT"            // Leaf.
 	FLOAT          = "FLOAT"          // Leaf.
 	STRING         = "STRING"         // Leaf.
