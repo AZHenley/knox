58927c466e4e44263fde0fc649c247f6f5be058a
Austin Henley <azh321@gmail.com>
2019-01-25 18:52:19 -0500

    Setup for building symtables.

--- symtable/symtable.go
+++ symtable/symtable.go
@@ -1,5 +1,16 @@
 package symtable
 
-// SymTable is a hash table for symbol entries.
+import "knox/ast"
+
+// SymTable is a table for symbol entries.
 type SymTable struct {
+	entries map[string]*ast.Node
+	parent  *SymTable
+}
+
+// New creates an initialized symbol table.
+func New(parentTable *SymTable) *SymTable {
+	s := &SymTable{}
+	s.entries = make(map[string]*ast.Node)
+	return s
 }
