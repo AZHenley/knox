6255a6a97b7b516343f4a87c7f8094184f801b99
Austin Henley <azh321@gmail.com>
2019-01-27 23:07:27 -0500

    Closes #41. Type checks expressions.

--- typechecker/typechecker.go
+++ typechecker/typechecker.go
@@ -3,6 +3,8 @@ package typechecker
 import (
 	"fmt"
 	"knox/ast"
+	"knox/lexer"
+	"knox/token"
 )
 
 // Analyze performs type checking on the entire AST.
@@ -29,7 +31,6 @@ func abortMsg(msg string) {
 // TODO: This should probably be split up into methods and a class for each AST node type.
 // Get type from expression node.
 func getType(node *ast.Node) string {
-	fmt.Println(node.Type)
 	switch node.Type {
 	case ast.BINARYOP:
 		left := getType(&node.Children[0])
@@ -37,11 +38,46 @@ func getType(node *ast.Node) string {
 		if left != right {
 			abortMsg("Mismatched types.")
 		}
-		if left != ast.INT && left != ast.FLOAT && left != ast.STRING {
+		if lexer.IsOperator([]rune(node.TokenStart.Literal)[0]) || node.TokenStart.Literal == ">=" || node.TokenStart.Literal == ">" || node.TokenStart.Literal == "<=" || node.TokenStart.Literal == "<" {
+			if left == ast.INT || left == ast.FLOAT {
+				return string(left)
+			} else if node.TokenStart.Type == token.PLUS && left == ast.STRING {
+				return string(left)
+			} else {
+				abortMsg("Invalid operation.")
+			}
+		} else if node.TokenStart.Literal == "&&" || node.TokenStart.Literal == "||" {
+			if left != ast.BOOL || right != ast.BOOL {
+				abortMsg("Invalid operation.")
+			}
+		}
+
+		return string(left)
+
+	case ast.UNARYOP:
+		single := getType(&node.Children[0])
+		if node.TokenStart.Type == token.BANG {
+			if single != ast.BOOL {
+				abortMsg("Invalid operation.")
+			}
+		}
+		if single != ast.INT && single != ast.FLOAT {
 			abortMsg("Invalid operation.")
 		}
-		return string(left)
-	case ast.INT, ast.FLOAT, ast.STRING, ast.BOOL:
+		return string(single)
+
+	case ast.VARREF:
+		// TODO
+		//name := node.Children[0].TokenStart.Literal
+		//isDeclared(name)
+		//look up type
+		return "INT"
+
+	case ast.FUNCCALL:
+		// TODO
+		return "INT"
+
+	case ast.INT, ast.FLOAT, ast.STRING, ast.BOOL, ast.NIL:
 		return string(node.Type)
 	}
 	return ""
