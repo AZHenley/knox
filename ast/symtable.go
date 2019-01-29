9dd9f2a1d779edab5f90726e3c58a4db63900e18
Austin Henley <azh321@gmail.com>
2019-01-28 21:52:16 -0500

    Closes #46 and #47. Refactored helper functions into symtable methods.

--- ast/symtable.go
+++ ast/symtable.go
@@ -14,3 +14,34 @@ func NewSymTable() *SymTable {
 	s.Entries = make(map[string]*Node)
 	return s
 }
+
+// InsertSymbol into symbol table, recursively checks if it exists, returns true for success
+func (st *SymTable) InsertSymbol(symbol string, node *Node) bool {
+	if st.IsDeclared(symbol) {
+		return false
+	}
+	st.Entries[symbol] = node
+	return true
+}
+
+// IsDeclared returns if symbol is declared, recursively.
+func (st *SymTable) IsDeclared(symbol string) bool {
+	if _, ok := st.Entries[symbol]; ok {
+		return true
+	}
+	if st.Parent == nil {
+		return false
+	}
+	return st.Parent.IsDeclared(symbol)
+}
+
+// LookupSymbol does a recursive lookup.
+func (st *SymTable) LookupSymbol(symbol string) *Node {
+	if node, ok := st.Entries[symbol]; ok {
+		return node
+	}
+	if st.Parent == nil {
+		return nil
+	}
+	return st.Parent.LookupSymbol(symbol)
+}
