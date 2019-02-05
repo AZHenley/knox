ddcfcb40ab18329d608298378e0bbdc4b5fbcbbf
Austin Henley <azh321@gmail.com>
2019-02-05 12:11:46 -0500

    Closes #67. Times duration of compiler phases.

--- knox.go
+++ knox.go
@@ -8,6 +8,7 @@ import (
 	"knox/lexer"
 	"knox/parser"
 	"knox/typechecker"
+	"time"
 )
 
 func main() {
@@ -15,14 +16,28 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	start := time.Now()
 	l := lexer.New(string(code))
 	p := parser.New(l)
 	a := p.Program()
+	elapsedParsing := time.Since(start)
 
 	ast.Print(a)
 
+	start = time.Now()
 	typechecker.Analyze(&a)
-	//cfa.Analyze(&a)
-	fmt.Print(emitter.Generate(&a))
+	elapsedTypeChecking := time.Since(start)
 
+	//cfa.Analyze(&a)
+
+	start = time.Now()
+	output := emitter.Generate(&a)
+	elapsedEmitting := time.Since(start)
+
+	fmt.Println(output)
+
+	fmt.Println(elapsedParsing)
+	fmt.Println(elapsedTypeChecking)
+	fmt.Println(elapsedEmitting)
 }
