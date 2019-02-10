33d00c079f5a07e73f47c569e32a39f9ce771af4
Austin Henley <azh321@gmail.com>
2019-02-10 12:10:03 -0500

    Refactored entire type checker to use type struct for #37.

--- typechecker/typechecker.go
+++ typechecker/typechecker.go
@@ -19,9 +19,33 @@ type typeObj struct {
 	inner       []typeObj // Inner types. TODO: Make this a slice of pointers of typeObj.
 }
 
+var typeBOOL *typeObj
+var typeINT *typeObj
+var typeFLOAT *typeObj
+var typeSTRING *typeObj
+var typeLIST *typeObj
+var typeMAP *typeObj
+var typeADDRESS *typeObj
+
 // Analyze performs type checking on the entire AST.
 func Analyze(node *ast.Node) {
+	setup()
 	typecheck(node)
+}
+
+func setup() {
+	typeBOOL = &typeObj{}
+	typeINT = &typeObj{}
+	typeFLOAT = &typeObj{}
+	typeSTRING = &typeObj{}
+	typeBOOL.isPrimitive = true
+	typeINT.isPrimitive = true
+	typeFLOAT.isPrimitive = true
+	typeSTRING.isPrimitive = true
+	typeBOOL.fullName = "bool"
+	typeINT.fullName = "int"
+	typeFLOAT.fullName = "float"
+	typeSTRING.fullName = "string"
 }
 
 func typecheck(node *ast.Node) {
@@ -32,8 +56,8 @@ func typecheck(node *ast.Node) {
 			// TODO: Handle for, return
 			if node.Type == ast.VARDECL {
 				// TODO: Handle multiple assignment.
-				leftType := declType(node)
-				if !compareTypes(leftType, exprType) { // Do the types match?
+				leftType := declType2(node)
+				if !compareTypes2(leftType, exprType) { // Do the types match?
 					abortMsg("Mismatched types.")
 				}
 			} else if node.Type == ast.VARASSIGN {
@@ -42,12 +66,12 @@ func typecheck(node *ast.Node) {
 				if decl == nil {
 					abortMsg("Referencing undeclared variable.")
 				}
-				leftType := declType(decl)
-				if !compareTypes(leftType, exprType) { // Do the types match?
+				leftType := declType2(decl)
+				if !compareTypes2(leftType, exprType) { // Do the types match?
 					abortMsg("Mismatched types.")
 				}
 			} else if node.Type == ast.IFSTATEMENT || node.Type == ast.WHILESTATEMENT {
-				if !compareTypes(exprType, ast.BOOL) {
+				if !compareTypes2(exprType, typeBOOL) {
 					abortMsg("Conditionals require boolean expressions.")
 				}
 			}
@@ -73,6 +97,16 @@ func compareTypes(a string, b string) bool {
 	return strings.ToLower(a) == strings.ToLower(b)
 }
 
+func compareTypes2(a *typeObj, b *typeObj) bool {
+	return a.fullName == b.fullName
+}
+
+func stringToType(prim string) *typeObj {
+	primitive := &typeObj{}
+	primitive.fullName = prim
+	return primitive
+}
+
 // Get type from a declaration.
 func declType(node *ast.Node) string {
 	if node.Type == ast.VARDECL {
@@ -80,54 +114,66 @@ func declType(node *ast.Node) string {
 	} else if node.Type == ast.FUNCDECL {
 		return node.Children[2].Children[0].Children[0].TokenStart.Literal // TODO: Handle multiple return values.
 	}
-	return ""
+	return "" // Should never happen?
 }
 
-// Get type from a symbol.
-func getSymbolType(symbol string, st *ast.SymTable) string {
-	node := st.LookupSymbol(symbol)
-	if node != nil {
-		return declType(node)
+// TODO: This needs to build up the typeobj.
+// Get type from a declaration.
+func declType2(node *ast.Node) *typeObj {
+	if node.Type == ast.VARDECL {
+		return stringToType(node.Children[1].Children[0].TokenStart.Literal)
+	} else if node.Type == ast.FUNCDECL {
+		return stringToType(node.Children[2].Children[0].Children[0].TokenStart.Literal) // TODO: Handle multiple return values.
 	}
-	return ""
+	abortMsg("Unknown type error.")
+	return nil
 }
+
+// // Get type from a symbol.
+// func getSymbolType(symbol string, st *ast.SymTable) string {
+// 	node := st.LookupSymbol(symbol)
+// 	if node != nil {
+// 		return declType(node)
+// 	}
+// 	return ""
+// }
 
 // Get type from expression node.
-func getType(node *ast.Node) string {
+func getType(node *ast.Node) *typeObj {
 	switch node.Type {
 	case ast.BINARYOP:
 		left := getType(&node.Children[0])
 		right := getType(&node.Children[1])
-		if !compareTypes(left, right) {
+		if !compareTypes2(left, right) {
 			abortMsg("Mismatched types.")
 		}
 		if lexer.IsOperator([]rune(node.TokenStart.Literal)[0]) || node.TokenStart.Literal == ">=" || node.TokenStart.Literal == ">" || node.TokenStart.Literal == "<=" || node.TokenStart.Literal == "<" {
-			if compareTypes(left, ast.INT) || compareTypes(left, ast.FLOAT) {
-				return string(left)
-			} else if node.TokenStart.Type == token.PLUS && compareTypes(left, ast.STRING) {
-				return string(left)
+			if compareTypes2(left, typeINT) || compareTypes2(left, typeFLOAT) {
+				return left
+			} else if node.TokenStart.Type == token.PLUS && compareTypes2(left, typeSTRING) {
+				return left
 			} else {
 				abortMsg("Invalid operation.")
 			}
 		} else if node.TokenStart.Literal == "&&" || node.TokenStart.Literal == "||" {
-			if !compareTypes(left, ast.BOOL) {
+			if !compareTypes2(left, typeBOOL) {
 				abortMsg("Invalid operation.")
 			}
 		}
 
-		return string(left)
+		return left
 
 	case ast.UNARYOP:
 		single := getType(&node.Children[0])
 		if node.TokenStart.Type == token.BANG {
-			if !compareTypes(single, ast.BOOL) {
+			if !compareTypes2(single, typeBOOL) {
 				abortMsg("Invalid operation.")
 			}
 		}
-		if !compareTypes(single, ast.INT) && !compareTypes(single, ast.FLOAT) {
-			abortMsg("4Invalid operation.")
+		if !compareTypes2(single, typeINT) && !compareTypes2(single, typeFLOAT) {
+			abortMsg("Invalid operation.")
 		}
-		return string(single)
+		return single
 
 	case ast.VARREF:
 		name := node.Children[0].TokenStart.Literal
@@ -135,7 +181,7 @@ func getType(node *ast.Node) string {
 		if declNode == nil {
 			abortMsg("Referencing undeclared variable.")
 		}
-		return declType(declNode)
+		return declType2(declNode)
 
 	case ast.FUNCCALL:
 		name := node.Children[0].TokenStart.Literal
@@ -143,14 +189,22 @@ func getType(node *ast.Node) string {
 		if declNode == nil {
 			abortMsg("Calling undeclared function.")
 		}
-		return declType(declNode)
+		return declType2(declNode)
 
-	case ast.INT, ast.FLOAT, ast.STRING, ast.BOOL, ast.NIL:
-		return string(node.Type)
+	case ast.INT:
+		return typeINT
+	case ast.FLOAT:
+		return typeFLOAT
+	case ast.STRING:
+		return typeSTRING
+	case ast.BOOL:
+		return typeBOOL
+	//case ast.NIL:
+	//return typeNIL
 
 	case ast.EXPRESSION:
 		return getType(&node.Children[0])
 	}
 
-	return ""
+	return nil
 }
